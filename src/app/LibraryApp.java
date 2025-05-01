ea72f013cf74140f0b1751e91dcecdbe06041390
Victor Hugo Fuly Silva <dev.victorfuly@gmail.com>
2025-05-01 18:04:32 -0300

    set up method Book.displayInfo() and book.isBorrowed()

--- src/app/LibraryApp.java
+++ src/app/LibraryApp.java
@@ -1,4 +1,10 @@
 package app;
 
+import domain.Book;
+
 public class LibraryApp {
+    public static void main(String[] args) {
+        Book bookItTheThing = new Book("It: The Thing", "Stephen King");
+        bookItTheThing.displayInfo();
+    }
 }
--- src/domain/Book.java
+++ src/domain/Book.java
@@ -3,20 +3,29 @@ package domain;
 public class Book {
     private String title;
     private String author;
-    private boolean isBorrowed;
+    private boolean borrowed;
 
-    public Book(String name, String author) {
-        this.title = name;
+    public Book(String title, String author) {
+        this.title = title;
         this.author = author;
-        isBorrowed = false;
+        borrowed = false;
+    }
+
+    public void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Borrow: " + (borrowed ? "Available" : "Unavailable"));
+    }
+    public boolean isBorrowed() {
+        return borrowed;
     }
 
     public void borrow() {
-        isBorrowed = true;
+        borrowed = true;
     }
 
     public void giveBack() {
-        isBorrowed = false;
+        borrowed = false;
     }
 
     public void setTitle(String title) {
