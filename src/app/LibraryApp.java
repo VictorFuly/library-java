5a74fe2b4ad0ad55c4042c892568bf1f3222bc9b
Victor Hugo Fuly Silva <dev.victorfuly@gmail.com>
2025-05-01 22:13:27 -0300

    created a searchBook() method

--- src/app/LibraryApp.java
+++ src/app/LibraryApp.java
@@ -7,18 +7,22 @@ public class LibraryApp {
     public static void main(String[] args) {
         Book bookItTheThing = new Book("It: The Thing", "Stephen King");
         Book bookHarryPotter = new Book("Harry Potter", "J. K. Rowling");
+        Book book1984 = new Book("1984", "George Orwell");
 
         /**
-        bookItTheThing.displayInfo();
-        bookHarryPotter.displayInfo();
-        **/
+         bookItTheThing.displayInfo();
+         bookHarryPotter.displayInfo();
+         **/
 
         Library libraryCoolBooks = new Library("Cool Books", "Washigton, 405");
-        libraryCoolBooks.displayInfo();
+        //libraryCoolBooks.displayInfo();
 
         libraryCoolBooks.addBookToCollection(bookItTheThing);
-        libraryCoolBooks.addBookToCollection(new Book("1984", "George Orwell"));
-        libraryCoolBooks.displayBookCollection();
+        libraryCoolBooks.addBookToCollection(bookHarryPotter);
+        libraryCoolBooks.addBookToCollection(book1984);
+
+        //libraryCoolBooks.displayBookCollection();
+        libraryCoolBooks.searchBook("it: the thing");
 
     }
 }
--- src/domain/Library.java
+++ src/domain/Library.java
@@ -25,27 +25,38 @@ public class Library {
         bookCollection.add(book);
     }
 
-    public void displayBookCollection() {
-        System.out.println("------ " + name + " Book Collection ------");
+    public void searchBook(String title) {
+        System.out.println("------ Search Result ------");
         for (Book book : bookCollection) {
-            book.displayInfo();
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                book.displayInfo();
+                return;
+            }
+        }
+        System.out.println("Result not found!");
+    }
+
+        public void displayBookCollection() {
+            System.out.println("------ " + name + " Book Collection ------");
+            for (Book book : bookCollection) {
+                book.displayInfo();
+            }
+        }
+
+
+        public void setName (String name){
+            this.name = name;
+        }
+
+        public String getName () {
+            return this.name;
+        }
+
+        public void setAddress (String address){
+            this.address = address;
+        }
+
+        public String getAddress () {
+            return this.address;
         }
     }
-
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getAddress() {
-        return this.address;
-    }
-}
